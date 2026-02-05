952f8485159fd4c20bc1205d7c7ebef1fc4285c3
Proboth Ravihara <raviharaproboth94@gmail.com>
2026-02-05 23:55:22 +0500

    Add resume PDF

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
-  base: "/react-home/", // 👈 MUST match repo name
+  base: "/proboth-portfolio/", // 👈 MUST match repo name
   plugins: [react(), tailwindcss()],
 });
