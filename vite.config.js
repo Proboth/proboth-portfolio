ea1e7ecf4f4b3b15021e478f9e02cafb03d27e98
Proboth Ravihara <raviharaproboth94@gmail.com>
2026-02-05 22:40:18 +0500

    Initial portfolio commit

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
-  base: "/portfolio/",   // ✅ GitHub repo name
+  base: "/react-home/",   // ✅ GitHub repo name
   plugins: [
     react(),
     tailwindcss(),
